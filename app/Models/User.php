62659f3600aeed2948a154a2700d583e1ba86b25
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-08-12 22:25:03 -0300

    trait added

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+Use Uspdev\SenhaunicaSocialite\Traits\HasSenhaunica;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, HasSenhaunica;
 
     /**
      * The attributes that are mass assignable.
